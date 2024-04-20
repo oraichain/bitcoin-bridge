1bd9fb0b5942b919b828e6d6fe59c92b54eba6f2
perfogic <longdieu12x@gmail.com>
2024-04-20 14:16:59 +0700

    update min_fee_rate to 200 due to halving

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 pub const MAIN_NATIVE_TOKEN_DENOM: &str = "uoraibtc";
 pub const BTC_NATIVE_TOKEN_DENOM: &str = "usat";
-pub const MIN_FEE_RATE: u64 = 20; // in satoshis per vbytes
+pub const MIN_FEE_RATE: u64 = 200; // in satoshis per vbytes
 pub const MAX_FEE_RATE: u64 = 1000; // in satoshis per vbytes
 pub const USER_FEE_FACTOR: u64 = 27000; // 2.7x. Calculate by USER_FEE_FACTOR / 10000
 pub const IBC_FEE: u64 = 0;
