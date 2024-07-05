84b5d3a66397ad7e3fb6008164e74eb6cbbca4eb
perfogic <longdieu12x@gmail.com>
2024-07-05 14:01:37 +0700

    update consensus-state

--- src/app.rs
+++ src/app.rs
@@ -105,7 +105,7 @@ pub struct InnerApp {
 
 #[orga]
 impl InnerApp {
-    pub const CONSENSUS_VERSION: u8 = 16;
+    pub const CONSENSUS_VERSION: u8 = 17;
 
     // #[cfg(feature = "full")]
     // fn configure_faucets(&mut self) -> Result<()> {
